afaa38379556f589ea54a8dd7fa19be8fad08850
otorquati <torquati.osvaldo@gmail.com>
2025-12-20 16:50:19 -0300

    feat: applied Lesson 6 exercise gd

---
+++ Exercises/Lesson6/ecommerce-project/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/HomePage.jsx';
+import { CheckoutPage } from './pages/Checkout/CheckoutPage.jsx';
+import { OrdersPage } from './pages/OrdersPage.jsx';
+import { TrackingPage } from './pages/TrackingPage.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+    </Routes>
+  )
+}
+export default App;
