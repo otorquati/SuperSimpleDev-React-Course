55825502566b3a4e47079ed854285ee8926ceab2
otorquati <torquati.osvaldo@gmail.com>
2025-12-22 10:49:05 -0300

    fix: change the Lesson6 exercise k as sollution

--- Exercises/Lesson6/ecommerce-project/src/pages/NotFoundPage.jsx
+++ Exercises/Lesson6/ecommerce-project/src/pages/NotFoundPage.jsx
@@ -1,16 +1,14 @@
 import { Link } from "react-router";
 import "./NotFoundPage.css";
+import { Header } from "../components/Header";
 export function NotFoundPage() {
   return (
     <>
+	<title>404 - Page Not Found</title>
+	<link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
+	<Header />
       <div className="notfoundpage-container">
-        <div className="notfoundpage-header">
-          <h1>404 - Page Not Found</h1>
-        </div>
         <p className="notfoundpage-container">The page you are looking for does not exist.</p>
-	  <button className="btn-home">
-	  <Link to="/" className="notfoundpage-home-link">Go back to Home Page</Link>
-	  </button>
       </div>
     </>
   );
