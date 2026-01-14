b5d2b0c469c34d58d1da3fe64dcdaea27151293c
otorquati <torquati.osvaldo@gmail.com>
2026-01-14 14:39:07 -0300

    feat: Make create order button work

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -35,7 +35,7 @@ export function CheckoutPage({ cart, loadCart }) {
             <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart} />
 
           {paymentSummary && (
-            <PaymentSummary paymentSummary={paymentSummary} />
+            <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
           )}
         </div>
       </div>
