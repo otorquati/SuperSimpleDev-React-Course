62f00845358f874233012cbf41a9396e59ff2798
djthorkuat <djthorkuat@gmail.com>
2026-02-07 10:50:00 -0300

    feat: Test clicking add to cart button

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,6 +1,10 @@
 import { test, expect, describe, vi } from "vitest";
 import { Product } from "./Product.jsx";
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
+import axios from "axios";
+// Mocka o módulo axios para evitar chamadas reais durante os testes
+vi.mock('axios');
 
 describe("Product component", () => {
   test("renders product details correctly", () => {
@@ -43,4 +47,41 @@ describe("Product component", () => {
     );
     expect(screen.getByText("87")).toBeInTheDocument();
   });
+  // Testando a interação do botão "Add to Cart"
+  test("calls loadCart function when 'Add to Cart' button is clicked", async () => {
+    const product = {      
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"],
+    };
+
+    // Cria um mock para a função loadCart, pois não podemos acessar o backend real durante o teste
+    const loadCart = vi.fn();
+
+    //renderiza o componente Product com os dados do produto e a função loadCart mockada
+    render(<Product product={product} loadCart={loadCart} />);
+
+    // Simula o clique no botão "Add to Cart"
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByTestId("add-to-cart-button");
+    await user.click(addToCartButton);
+
+    // Verifica se a função axios.post foi chamada com os parâmetros corretos
+    expect(axios.post).toHaveBeenCalledWith(
+      '/api/cart-items',
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1,
+      }
+    )
+    
+    // Verifica se a função loadCart foi chamada
+    expect(loadCart).toHaveBeenCalled();
+    })
 });
